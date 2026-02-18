eec79c78d3792ce547f6035942980f05381c03fa
PiotrekS2 <rozenpiotrek5@gmail.com>
2026-02-18 10:27:54 +0100

    zadania php 11

--- zadania_php.php
+++ zadania_php.php
@@ -146,11 +146,11 @@ if ($rok < 1582)
 } 
 else if ($rok % 4 == 0 && $rok % 100 != 0 || $rok % 400 == 0)
  {
-        echo "Luty ma 29 dni.";
+        echo "Rok jest przestepny";
  }
      else
          {
-        echo "Luty ma 28 dni.";
+        echo "Rok nie jest przestepny";
     }
 
 
