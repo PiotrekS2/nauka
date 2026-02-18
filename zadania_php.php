239473feef69a7f415b57dcb24772e8e15caca1b
PiotrekS2 <rozenpiotrek5@gmail.com>
2026-02-18 10:27:14 +0100

    zadania php 10

--- zadania_php.php
+++ zadania_php.php
@@ -57,6 +57,8 @@ if($d >= 1 && $d<=10)
         echo' liczba jest z przedzailu <17,21>>';
     }
 
+
+
 echo'<br>';
 echo'<br>';
 echo'zadanie4:';
@@ -64,14 +66,92 @@ echo'<br>';
 echo'<br>';
 
 
+$e=7;
+
+if($e > 0)
+    {
+        echo'liczba jest wieksza od zera';
+    }
+ else if($e < 0)
+    {
+        echo'liczba jest mniejsza od zera';
+    }
+    else
+        {
+            echo'liczba jest rowan zero';
+        }
+
+
+
+echo'<br>';
+echo'<br>';
+echo'zadanie 5:';
+echo'<br>';
+echo'<br>';
+
+$wiek= 18;
+if($wiek < 11)
+    {
+        echo'dziecko';
+    }
+    else if($wiek > 11 && $wiek <17)
+        {
+            echo'nastolatek';
+        }
+        else
+            {
+                echo'dorosly';
+            }
+
+echo'<br>';
+echo'<br>';
+echo'zadanie 6:';
+echo'<br>';
+echo'<br>';
+
+
+$haslo='1234567';
+if(strlen($haslo) < 5)
+    {
+        echo'bardzo slabe';
+    }
+    else if(strlen($haslo) >= 5 && strlen($haslo) < 8)
+        {
+            echo'slabe';
+        }
+        else if(strlen($haslo) >=9 && strlen($haslo) < 11)
+            {
+                echo'silne';
+            }
+            else
+                {
+                    echo'bardzo silne';
+                }
+
+
+echo'<br>';
+echo'<br>';
+echo'zadanie 7:';
+echo'<br>';
+echo'<br>';
 
 
 
 
 
-
-
-
+$rok=1993;
+if ($rok < 1582) 
+    {
+    echo "Program obsługuje lata od 1582 roku.";
+} 
+else if ($rok % 4 == 0 && $rok % 100 != 0 || $rok % 400 == 0)
+ {
+        echo "Luty ma 29 dni.";
+ }
+     else
+         {
+        echo "Luty ma 28 dni.";
+    }
 
 
 ?>
